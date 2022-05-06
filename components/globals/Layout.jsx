c9587de905bc1d2cc85f9b3109b3a64c94f1ce3a
junteudjio <jun.teudjio@gmail.com>
2022-05-06 22:58:46 +0100

    add components

--- components/globals/Layout.jsx
+++ components/globals/Layout.jsx
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <main className='max-w-7xl mx-auto sm:px-6 lg:px-8 bg-slate-50 m-2 p-2'>
+      <main className='max-w-screen-2xl	 mx-auto sm:px-6 lg:px-8 bg-slate-50 m-2 p-2'>
         {children}
       </main>
       <Footer />
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import { getJobs } from '../datalayer';
 import JobsList from '../components/data/lists/JobsList';
+import SearchJobForm from '../components/forms/SearchJobForm';
+import JobsPageSideBarForm from '../components/forms/JobsPageSideBarForm';
+import JobsPageHeader from '../components/ui/JobsPageHeader';
 
 export default function Index({ jobs }) {
-  return <JobsList jobs={jobs} />;
+  return (
+    <div className='flex flex-col space-y-10 sm:flex-row sm:space-x-6 sm:space-y-0 md:flex-col md:space-x-0 md:space-y-10 xl:flex-row xl:space-x-6 xl:space-y-0 mt-9'>
+      <JobsPageSideBarForm />
+
+      <div className='w-full'>
+        <SearchJobForm />
+        <JobsPageHeader jobs={jobs} />
+        <JobsList jobs={jobs} />
+      </div>
+    </div>
+  )
 }
 
 export const getStaticProps = async (ctx) => {
