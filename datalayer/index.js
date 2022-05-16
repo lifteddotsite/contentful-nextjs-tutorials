295b3e4674a2fc4c49cefd6006fd252a114a8bb5
junteudjio <jun.teudjio@gmail.com>
2022-05-16 12:28:28 +0100

    merge main branch final touches

--- datalayer/index.js
+++ datalayer/index.js
@@ -2,13 +2,11 @@ let datasource = {};
 
 import * as strapiJobAPI from './contentful/job';
 import * as strapiCompanyAPI from './contentful/company';
-
 if (process.env.DATALAYER_ENGINE === 'contentful')
   datasource = { ...contentfulCompanyAPI, ...contentfulJobAPI };
 
 import * as contentfulJobAPI from './strapi/job';
 import * as contentfulCompanyAPI from './strapi/company';
-
 if (process.env.DATALAYER_ENGINE === 'strapi')
   datasource = { ...strapiCompanyAPI, ...strapiJobAPI };
 
